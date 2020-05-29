562f6ac4138e7cd5cffa67d8a532a3c1a08cb169
kiran <kiran111candy@gmail.com>
2020-05-29 09:34:35 -0400

    Updates

---
+++ precache-manifest.bd405d7034802fe621401ffb33bb95f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80dd90fe76f345f1df839fcca0023ef2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36ff4bfe5564c51a4176",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1a66b78346db458e3fc5",
+    "url": "/static/css/main.6837c351.chunk.css"
+  },
+  {
+    "revision": "36ff4bfe5564c51a4176",
+    "url": "/static/js/2.02b0ba98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.02b0ba98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a66b78346db458e3fc5",
+    "url": "/static/js/main.aef0cc5e.chunk.js"
+  },
+  {
+    "revision": "9f1e78d60d8b52bd3d90",
+    "url": "/static/js/runtime-main.a86148e1.js"
+  }
+]);
